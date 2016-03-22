c669f31f3aa89fd87e9ae8f724c77aecfe4ad4d3
Denis Glebov <dionis.mgn@gmail.com>
2016-03-23 02:56:03 +0500

    Correct server-side 'letsgo' implementation

--- letsgo.php
+++ letsgo.php
@@ -1,10 +1,21 @@
 <?php
 
 define("TIMESTAMP_FILENAME", "/usr/html/letsgodata");
+define("TIMEOUT", 120);
+
+$oldTimestamp = file_get_contents(TIMESTAMP_FILENAME);
 
 $timestamp = time();
+
+if ($timestamp - $oldTimestamp < TIMEOUT)
+{
+	http_response_code(500);
+	die("Busy");
+}
+
 if (!file_put_contents(TIMESTAMP_FILENAME, $timestamp))
 {
+	http_response_code(500);
 	die("Ooops");
 }
 else {
